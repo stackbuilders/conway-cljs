b34f57a634e2c87776cfba0157087b32720bf6c3
Sebastian Sogamoso <sebasoga@gmail.com>
2013-02-05 10:44:06 -0500

    Add a few styles and make table bigger * Added a few styles to center the table an tittle. * Made the table 25x25

--- src-clj/conway/views.clj
+++ src-clj/conway/views.clj
@@ -4,18 +4,18 @@
       [page :refer [html5]]
       [page :refer [include-js]]]))
 
-(defn square-board 
-  [size] 
-  (vec (into [:table] 
-             (replicate size (vec 
+(defn square-board
+  [size]
+  (vec (into [:table]
+             (replicate size (vec
                               (into [:tr] (replicate size [:td "&nbsp;"])))))))
 
 (defn index-page []
   (html5
     [:head
-     [:style "td{ width: 20px; }"]
-     [:title "Conway"]
+     [:style "h1{ text-align: center; } table{ margin: auto; } td{ width: 20px; }"]
+     [:title "Conway's Game of Life"]
      (include-js "/js/main.js")]
     [:body
-      [:h1 "Conway"]
-     (square-board 20)]))
+      [:h1 "Conway's Game of Life"]
+     (square-board 25)]))
